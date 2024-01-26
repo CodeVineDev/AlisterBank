520ce4f0888f8dde1aaff86e8e2def12e34bc5f8
Chukwudolue Divine <codevine2023@gmail.com>
2024-01-27 00:39:32 +0100

    Navbar Edit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // GOLDEN JAVASCRIPT CODE 
+// FOR THE IMAGE SLIDER
 var counter = 1;
 setInterval(function () {
     document.getElementById('radio' + counter).checked = true;
@@ -7,3 +8,20 @@ setInterval(function () {
         counter = 1;
     }
 }, 7000)
+
+// NAVBAR 
+let sidebarBtn = document.querySelector('#sidebarBtn');
+let closeModal = document.querySelector('#close-modal');
+let sideBar = document.querySelector('.sideBar')
+let bodySlider = document.querySelector('.body-slider');
+
+sidebarBtn.addEventListener('click', () => {
+    sideBar.style.top = "60px"
+    sidebarBtn.style.opacity = 0;
+    bodySlider.style.marginTop = "315px"
+})
+closeModal.addEventListener('click', () => {
+    sideBar.style.top = "-305px"
+    sidebarBtn.style.opacity = 1;
+    bodySlider.style.marginTop = 0
+})
