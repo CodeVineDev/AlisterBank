d91404771c4ab29c50cdd68d1e98c97a95ed5035
Chukwudolue Divine <codevine2023@gmail.com>
2024-03-05 16:22:58 +0100

    styled navbar responsiveness

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,13 +1,15 @@
-// window.addEventListener('scroll', () => {
-//     let navbar = document.getElementById('navbar');
-//     let height = navbar.offsetHeight;
-  
-//     if (window.scrollY > height) {
-//       navbar.style.top = '0';
-//       navbar.classList.add('sticky');
-//     } else {
-//       navbar.style.top = '-' + height + 'px';
-//       navbar.classList.remove('sticky');
-//     }
-//   });
-  
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  let dropnavBtn = document.querySelector('.dropnav-btn');
+  let dropnavContainer = document.querySelector('.dropnav-container');
+  let dropnavBars = document.querySelector('.bars');
+
+  dropnavBtn.addEventListener('click', () => {
+    dropnavContainer.classList.toggle('active')
+  })
+
+ 
+});
+
+
