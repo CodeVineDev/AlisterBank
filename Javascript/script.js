c0b375a3560e6c239f9e8c577f04d022d37e3ca5
Chukwudolue Divine <codevine2023@gmail.com>
2024-03-11 22:32:45 +0100

    What people have to say section

--- Javascript/script.js
+++ Javascript/script.js
@@ -12,9 +12,19 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
- // Generate a random number
- var randomNumber = Math.floor(Math.random() * 1000000);
+// Generate a random number
+var randomNumber = Math.floor(Math.random() * 1000000);
 
- // Update the CSS link with the random number as a cache-busting parameter
- document.querySelector('link[rel="stylesheet"]').href = './Dist/css/main.css?v=' + randomNumber;
+// Update the CSS link with the random number as a cache-busting parameter
+document.querySelector('link[rel="stylesheet"]').href = './Dist/css/main.css?v=' + randomNumber;
+
+let youClass = document.querySelector('.testimonial-slider')
+$(document).ready(function () {
+  $('youClass').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+  });
+});
 
