71b0ac997ae887a8d0e950f130d98eff8be476da
Chukwudolue Divine <codevine2023@gmail.com>
2024-03-12 20:20:54 +0100

    Try to add card slider

--- Javascript/script.js
+++ Javascript/script.js
@@ -17,14 +17,32 @@ var randomNumber = Math.floor(Math.random() * 1000000);
 
 // Update the CSS link with the random number as a cache-busting parameter
 document.querySelector('link[rel="stylesheet"]').href = './Dist/css/main.css?v=' + randomNumber;
-
-let youClass = document.querySelector('.testimonial-slider')
-$(document).ready(function () {
-  $('youClass').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-  });
+$(document).ready(function(){
+    $('.testimonial-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // Add more responsive settings as needed
+        ]
+    });
 });
 
+	
