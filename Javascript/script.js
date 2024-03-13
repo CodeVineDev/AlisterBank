8d25639571777dafc702239d676c50443abbcca9
Chukwudolue Divine <codevine2023@gmail.com>
2024-03-13 20:28:40 +0100

    debugging

--- Javascript/script.js
+++ Javascript/script.js
@@ -17,32 +17,36 @@ var randomNumber = Math.floor(Math.random() * 1000000);
 
 // Update the CSS link with the random number as a cache-busting parameter
 document.querySelector('link[rel="stylesheet"]').href = './Dist/css/main.css?v=' + randomNumber;
-$(document).ready(function(){
-    $('.testimonial-slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots: true,
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-            // Add more responsive settings as needed
-        ]
-    });
+
+
+
+$(document).ready(function () {
+  $('.testimonial-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // Add more responsive settings as needed
+    ]
+  });
 });
 
-	
+
