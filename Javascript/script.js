13a33cc314629099b7d38f297400f5c2ec86b27b
Chukwudolue Divine <codevine2023@gmail.com>
2024-07-28 21:46:34 +0100

    Total update

--- Javascript/script.js
+++ Javascript/script.js
@@ -26,40 +26,4 @@ document.querySelector('link[rel="stylesheet"]').href = './Dist/css/main.css?v='
 
 
 
-$(document).ready(function () {
-  $('.testimonial-slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 4,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    dots: true,
-    arrows: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 850,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 550,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }]
-  });
-});
-
 
