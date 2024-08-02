a670899f143d7879e10ce940b3072e08e7cf9b45
Chukwudolue Divine <codevine2023@gmail.com>
2024-08-02 14:03:25 +0100

    Navbar dropcontainer

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,26 +1,25 @@
-// NAVBAR SCRIPT
 document.addEventListener("DOMContentLoaded", () => {
-  let dropnavBtn = document.querySelector('.dropnav-btn');
-  let dropnavContainer = document.querySelector('.dropnav-container');
-
-  dropnavBtn.addEventListener('click', () => {
-    dropnavContainer.classList.toggle('active')
-    dropnavBtn.classList.toggle("toggle");
-
-  })
+  const dropnavBtns = document.querySelectorAll('.dropnav-btn');
+  
+  dropnavBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      const targetSelector = btn.getAttribute('data-target');
+      const container = document.querySelector(targetSelector);
+      
+      if (container) {
+        container.classList.toggle('active');
+        btn.classList.toggle('toggle');
+      }
+    });
+  });
 });
+
+
 window.addEventListener('load', () => {
   window.scrollTo(0, 0);  // Scrolls to the top left corner of the page 
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var dropnavBtn = document.querySelector(".dropnav-btn");
 
-  dropnavBtn.addEventListener("click", function () {
-    var dropnavContainer = dropnavContainer.classList.toggle("active");
-    dropnavBtn.classList.toggle("toggle");
-  });
-});
 
 
 document.addEventListener("DOMContentLoaded", () => {
